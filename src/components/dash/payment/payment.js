5d53a6f922b506eceea37dc1c8ce161f657f3d50
Hassan Bello <tigerbello5@yahoo.com>
2020-08-20 03:11:09 +0100

    Payment Init

---
+++ src/components/dash/payment/payment.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import * as Styles from './payment.css'
+
+const Payment = (props) => {
+
+    return (
+        <div>
+
+        </div>
+    );
+
+}
+
+export default Payment;
---
+++ src/components/dash/payment/paymentTable.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import * as Styles from './payment.css'
+
+const PaymentTable = (props) => {
+    return (
+        <div>Snip Snap</div>
+    );
+}
+
+export default PaymentTable;
