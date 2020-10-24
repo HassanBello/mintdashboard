661ba43dfe9e2d8f1d7eb4825dbd555425e89ce6
Hassan Bello <tigerbello5@yahoo.com>
2020-10-24 20:11:53 +0100

    Updated Values

--- src/components/dash/payment/payment.js
+++ src/components/dash/payment/payment.js
@@ -7,29 +7,29 @@ const Payment = (props) => {
     const tableData = [
         {
             itemType: "Apple Mac Book 15” 250 SSD 12GB",
-            price: "$7,3430",
-            time: "12:30",
+            price: "$1,3430",
+            time: "15:30",
             transactionNo: "1234567890",
             status: "Pending"
         },
         {
             itemType: "Apple Mac Book 15” 250 SSD 12GB",
-            price: "$7,3430",
+            price: "$70,3430",
             time: "12:30",
             transactionNo: "1234567890",
             status: "Reconciled"
         },
         {
             itemType: "Apple Mac Book 15” 250 SSD 12GB",
-            price: "$7,3430",
+            price: "$5,3430",
             time: "12:30",
             transactionNo: "1234567890",
-            status: "Pending"
+            status: "Reconciled"
         },
         {
             itemType: "Apple Mac Book 15” 250 SSD 12GB",
-            price: "$7,3430",
-            time: "12:30",
+            price: "$2,3430",
+            time: "14:30",
             transactionNo: "1234567890",
             status: "Reconciled"
         }
